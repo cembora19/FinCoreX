3df9488113528b1a6decb0bd25863fa766cee339
cembora19 <cemboraceylandev@gmail.com>
2026-05-19 18:45:55 +0300

    feat: initialize spring boot project

---
+++ src/main/java/com/fincorex/FincorexApplication.java
@@ -0,0 +1,13 @@
+package com.fincorex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FincorexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FincorexApplication.class, args);
+	}
+
+}
